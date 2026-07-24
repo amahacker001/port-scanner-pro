c47c07f24697fb685ad7cdf525c077f4bdf78f30
amahacker001 <aliyu09013852501@gmail.com>
2026-07-24 01:41:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+from utils.banner import banner
 from scanner.port_scanner import scan_ports
 from scanner.web_info import get_web_info
-from utils.banner import banner
+from scanner.subdomain_finder import find_subdomains
+from scanner.dir_scanner import scan_directories
 
 banner()
 
 target = input("Enter target (domain/IP): ")
 
-print("\n[1] Port Scan")
-print("[2] Web Info Scan")
+print("""
+[1] Port Scan
+[2] Web Info
+[3] Subdomain Finder
+[4] Directory Scanner
+""")
 
-choice = input("Choose option: ")
+choice = input("Select option: ")
 
 if choice == "1":
     start = int(input("Start port: "))
@@ -18,6 +24,12 @@ if choice == "1":
 
 elif choice == "2":
     get_web_info(target)
+
+elif choice == "3":
+    find_subdomains(target)
+
+elif choice == "4":
+    scan_directories(target)
 
 else:
     print("Invalid choice")
